bcaa7d306bdcd5ab7363f391e4f45e599be60d21
adam <adamharits25@gmail.com>
2022-02-22 12:30:22 +0700

    updated cta

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php 
 
-$mysqli = new mysqli('localhost', 'u770127161_IsmiDki', '', 'u770127161_AnggotaBaru');
+$mysqli = new mysqli('localhost', 'u770127161_IsmiDki', 'IsmiDki54321', 'u770127161_AnggotaBaru');
 if($mysqli -> connect_errno) {
     die("Connection Failed: " . $mysqli->connect_error);
 }
