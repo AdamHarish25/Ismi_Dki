f990acd05b0516857aff724e4d57dd9bfc0560c4
adam <adamharits25@gmail.com>
2022-02-23 07:51:51 +0700

    second commit

--- connection.php
+++ connection.php
@@ -1,8 +1,14 @@
-<?php 
-
-$mysqli = new mysqli('localhost', 'u770127161_IsmiDki', 'IsmiDki54321', 'u770127161_AnggotaBaru');
-if($mysqli -> connect_errno) {
-    die("Connection Failed: " . $mysqli->connect_error);
+<?php
+$servername = "localhost";
+$database = "u770127161_AnggotaBaru";
+$username = "u770127161_IsmiDki";
+$password = "IsmiDki54321";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-
+echo "Connected successfully";
+mysqli_close($conn);
 ?>
