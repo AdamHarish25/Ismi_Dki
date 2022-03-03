d5c5060695b60189698ff4a5ba2b4fa8d7a11634
adam <adamharits25@gmail.com>
2022-03-03 09:48:30 +0700

    Added recently

--- connection.php
+++ connection.php
@@ -1,14 +1,10 @@
 <?php
-$servername = "localhost";
-$database = "u770127161_AnggotaBaru";
-$username = "u770127161_IsmiDki";
-$password = "IsmiDki54321";
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+try {    
+    //create PDO connection 
+    $db = new PDO("mysql:host=217.21.74.189;dbname=u770127161_AnggotaBaru", 'u770127161_IsmiDki', 'IsmiDki54321');
+} catch(PDOException $e) {
+    //show error
+    die("Terjadi masalah: " . $e->getMessage());
 }
-echo "Connected successfully";
-mysqli_close($conn);
 ?>
